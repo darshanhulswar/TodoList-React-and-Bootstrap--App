5598da94661cfbebb42b78c4f234d0a12e9d80c4
Darshan <dhulswar591@gmail.com>
2020-10-11 21:56:00 +0530

    Course Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './Person/Person';
+import Person from './Person/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Here I'm Darshan</h1>
+      <Person />
+    </div>
+  );
+}
+
+export default App;
