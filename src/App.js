bb5aedd049727f5dde1efdcf526e2ee8d0233ef1
Darshan <dhulswar591@gmail.com>
2020-11-02 16:08:52 +0530

    Header Component Integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Header from './Components/Header/Header'
 
 export class App extends Component {
   render() {
     return (
       <div>
-
+        <Header />
       </div>
     )
   }
