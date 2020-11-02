e325758dbfacf5f500c9fb1d6c6beb5cd07d310a
Darshan <dhulswar591@gmail.com>
2020-11-02 19:36:56 +0530

    app.js modification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Header from './Components/Header/Header'
+import AddTodo from './Components/AddTodo/AddTodo'
+
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <AddTodo />
       </div>
     )
   }
