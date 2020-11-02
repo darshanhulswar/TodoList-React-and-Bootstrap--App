72412891283a53487aecc75178a62554af9b919a
Darshan <dhulswar591@gmail.com>
2020-11-02 16:05:42 +0530

    Header setup

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Header() {
+    return (
+        <header>
+            <div className="container-fluid bg-light">
+                <h2 className="display-4 text-center text-primary py-3">To-Do-Tasks</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
